e1630efbbdd59034596ad2dcd24bd0a692dc038c
Jakub Hrkal <jakhrkal@gmail.com>
2023-02-23 15:58:16 +0100

    Kutulu - Wood 2 - Find shortest paths avoid closest wanderer

--- Code-of-Kutulu.ts
+++ Code-of-Kutulu.ts
@@ -10,6 +10,7 @@ class Position {
         public y: number
     ) { }
     equals(other: Position): boolean {
+        if (!other) { return false; }
         return this.x === other.x && this.y === other.y;
     }
     neighbour(dir: Direction): Position {
@@ -38,8 +39,59 @@ class Wanderer extends Entity {
     target: number;
 }
 
-const width: number = parseInt(readline());
-const height: number = parseInt(readline());
+class Path {
+    constructor(public start: Position) { }
+    private firstStep = new Step(null, this.start);
+    depth = 0;
+    visitedPositions: Step[] = [this.firstStep];
+    activeSteps = [this.firstStep];
+
+    visited(pos: Position): boolean {
+        return this.visitedPositions.some(existing => existing.pos.equals(pos));
+    }
+
+    nextStep() {
+        const nextActiveSteps: Step[] = [];
+        this.depth++;
+        this.activeSteps.forEach(activeStep => {
+            const positions = activeStep.getAvailableNextPositions();
+            positions
+                .filter(pos => !this.visited(pos))
+                .forEach(newlyReachablePos => {
+                    if (!nextActiveSteps.some(step => step.pos.equals(newlyReachablePos))) {
+                        const newStep = new Step(activeStep, newlyReachablePos);
+                        this.visitedPositions.push(newStep);
+                        nextActiveSteps.push(newStep);
+                    }
+                });
+        })
+        this.activeSteps = nextActiveSteps;
+    }
+
+    // Only path. Exclude source and target themselves
+    reconstructPathTo(pos: Position): Position[] | null {
+        let currentStep = this.visitedPositions.find(step => step.pos.equals(pos));
+        if (currentStep) {
+            let result: Position[] = [];
+            while (currentStep.backStep != null && !currentStep.backStep.pos.equals(this.firstStep.pos)) {
+                result.unshift(currentStep.backStep.pos);
+                currentStep = currentStep.backStep;
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+}
+
+class Step {
+    constructor(public backStep: Step | null, public pos: Position) { }
+    getAvailableNextPositions(): Position[] {
+        return getAvailableDirections(this.pos).map(dir => this.pos.neighbour(dir));
+    }
+}
+
+interface EntityPath<T extends Entity> { entity: T, path: Position[] }
 
 // # : wall
 // w : spawn for wanderers
@@ -47,9 +99,13 @@ const height: number = parseInt(readline());
 const map: string[] = [];
 
 let me: Explorer;
-let lastDirection = Direction.Up;
 let otherExplorers: Explorer[] = [];
 let wanderers: Wanderer[] = [];
+
+
+// READ GAME DATA
+const width: number = parseInt(readline());
+const height: number = parseInt(readline());
 
 for (let i = 0; i < height; i++) {
     const line: string = readline();
@@ -62,7 +118,7 @@ const sanityLossGroup: number = parseInt(inputs[1]); // how much sanity you lose
 const wandererSpawnTime: number = parseInt(inputs[2]); // how many turns the wanderer take to spawn, always 3 until wood 1
 const wandererLifeTime: number = parseInt(inputs[3]); // how many turns the wanderer is on map after spawning, always 40 until wood 1
 
-// game loop
+// GAME LOOP
 while (true) {
 
     // RESET GAME DATA
@@ -85,17 +141,31 @@ while (true) {
     }
 
     // PLAY !!!
-    const directions = getAvailableDirections();
-    if (directions.length > 0) {
-        let selectedDirection: Direction;
-        if (directions.includes(lastDirection)) {
-            selectedDirection = lastDirection;
-        } else {
-            selectedDirection = directions[0];
-        }
-        lastDirection = selectedDirection;
-        const selectedDest = myPosition().neighbour(selectedDirection);
-        console.log('MOVE ' + selectedDest.x + ' ' + selectedDest.y);
+    const pathsToExplorers: EntityPath<Explorer>[] = getExplorers()
+        .map(ex => calculateEntityPath(myPosition(), ex))
+        .filter(item => item.path.length > 0)
+        .sort((a, b) => a.path.length - b.path.length);
+    const pathsToWanderers: EntityPath<Wanderer>[] = getWanderers()
+        .map(w => calculateEntityPath(myPosition(), w))
+        .filter(item => item.path.length > 0)
+        .sort((a, b) => a.path.length - b.path.length);
+
+
+    const nearestExplorer = pathsToExplorers[0];
+    const nearestWanderer = pathsToWanderers[0];
+
+    let pos;
+
+    if (nearestExplorer) {
+        pos = nearestExplorer.path[0];
+    }
+
+    if (nearestWanderer && nearestWanderer.path.length < 3) {
+        pos = awayFrom(myPosition(), nearestWanderer.path[0]);
+    }
+
+    if (pos && !pos.equals(myPosition())) {
+        console.log('MOVE ' + pos.x + ' ' + pos.y);
     } else {
         console.log('WAIT');
     }
@@ -127,14 +197,62 @@ function myPosition(): Position {
     return me.pos;
 }
 
-// DECISIONS
-function getAvailableDirections(): Direction[] {
-    const result: Direction[] = [];
-    if (isFreeSpace(myPosition().neighbour(Direction.Up))) { result.push(Direction.Up) }
-    if (isFreeSpace(myPosition().neighbour(Direction.Right))) { result.push(Direction.Right) }
-    if (isFreeSpace(myPosition().neighbour(Direction.Down))) { result.push(Direction.Down) }
-    if (isFreeSpace(myPosition().neighbour(Direction.Left))) { result.push(Direction.Left) }
+// DISTANCES
+function calculateEntityPath<T extends Entity>(a: Position, b: T): EntityPath<T> {
+    return { entity: b, path: calculatePathTo(a, b) };
+}
+
+function calculatePathTo(a: Position, b: Entity): Position[] {
+    if (!b || a.equals(b.pos)) {
+        return [];
+    }
+
+    const pathsFromSource = new Path(a);
+    const pathsFromTarget = new Path(b.pos);
+    while (!haveInterjection(pathsFromSource, pathsFromTarget) && pathsFromSource.depth < 6) {
+        pathsFromSource.nextStep();
+        pathsFromTarget.nextStep();
+    }
+
+    const interjection = getInterjection(pathsFromSource, pathsFromTarget);
+    const result: Position[] = []
+    if (interjection) {
+        const stepsFromSource = pathsFromSource.reconstructPathTo(interjection.pos);
+        const stepsFromTarget = pathsFromTarget.reconstructPathTo(interjection.pos);
+
+        stepsFromSource?.forEach(step => result.push(step));
+        result.push(interjection.pos);
+        stepsFromTarget?.reverse()?.forEach(step => result.push(step));
+        if (!interjection.pos.equals(b.pos)) {
+            result.push(b.pos);
+        }
+    }
     return result;
+}
+
+function getInterjection(a: Path, b: Path): Step | undefined {
+    if (!a || !b) {
+        return undefined;
+    }
+    return a.visitedPositions.find(src => b.visited(src.pos));
+}
+
+function haveInterjection(a: Path, b: Path): boolean {
+    return getInterjection(a, b) !== undefined;
+}
+
+// DECISIONS
+function getAvailableDirections(pos: Position): Direction[] {
+    const result: Direction[] = [];
+    if (isFreeSpace(pos.neighbour(Direction.Up))) { result.push(Direction.Up) }
+    if (isFreeSpace(pos.neighbour(Direction.Right))) { result.push(Direction.Right) }
+    if (isFreeSpace(pos.neighbour(Direction.Down))) { result.push(Direction.Down) }
+    if (isFreeSpace(pos.neighbour(Direction.Left))) { result.push(Direction.Left) }
+    return result;
+}
+
+function awayFrom(pos: Position, thread: Position): Position {
+    return getAvailableDirections(pos).map(dir => pos.neighbour(dir)).filter(pos => !pos.equals(thread))[0] || thread;
 }
 
 function isFreeSpace(pos: Position) {
